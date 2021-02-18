b7a24cb98b6efe5703937623ca2d62be04b73aa0
Jacob Säll Nilsson <jacob.e.s.nilsson@gmail.com>
2021-02-18 13:09:52 +0100

    updated build to build of theme2

---
+++ build/precache-manifest.abbd4a9dcc367d2b16ba7ce916977fb7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3555525af16c48c21714a3f3c284d3d9",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "ff88c28c6eb0e51894a6",
+    "url": "/2020/static/css/main.730eae06.chunk.css"
+  },
+  {
+    "revision": "ddfe96a1a9c1ca6caeb9",
+    "url": "/2020/static/js/2.f249c56c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/2020/static/js/2.f249c56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff88c28c6eb0e51894a6",
+    "url": "/2020/static/js/main.fb9f9e71.chunk.js"
+  },
+  {
+    "revision": "62358d8dc03565bd9a7b",
+    "url": "/2020/static/js/runtime-main.af42a638.js"
+  },
+  {
+    "revision": "84c84e375b019eb26a42d4394c8a7365",
+    "url": "/2020/static/media/D-logo.84c84e37.png"
+  },
+  {
+    "revision": "1ca6bd5955073a5642f827d9e1da8fc8",
+    "url": "/2020/static/media/LTH.1ca6bd59.jpg"
+  },
+  {
+    "revision": "a8a1d6316152f9112fb9b1832e2e0405",
+    "url": "/2020/static/media/Shadowfield DEMO.a8a1d631.otf"
+  },
+  {
+    "revision": "034674d90bd31efa739120c2b03416f5",
+    "url": "/2020/static/media/Skärmklipp.034674d9.PNG"
+  },
+  {
+    "revision": "3e23cd947f098d01a16067efc17da9ae",
+    "url": "/2020/static/media/TLTH-01.3e23cd94.jpg"
+  },
+  {
+    "revision": "7dd8a52b5dfdb17194b960daee4936de",
+    "url": "/2020/static/media/delphi_vy.7dd8a52b.jpg"
+  },
+  {
+    "revision": "bd64455e5c0ed7d18b035225ef53741f",
+    "url": "/2020/static/media/g.bd64455e.jpg"
+  },
+  {
+    "revision": "dda7d6d10261bddc63aa2299074f72a7",
+    "url": "/2020/static/media/j.dda7d6d1.jpg"
+  },
+  {
+    "revision": "592bde7236df270c392cba67fa9adea7",
+    "url": "/2020/static/media/j2.592bde72.jpg"
+  },
+  {
+    "revision": "92e3650ae82b40adae96331136d839c0",
+    "url": "/2020/static/media/logo2.92e3650a.png"
+  },
+  {
+    "revision": "6da6692ae9f391cf726742509a48af09",
+    "url": "/2020/static/media/m.6da6692a.jpg"
+  },
+  {
+    "revision": "48907311ece93aad418aa589534ad121",
+    "url": "/2020/static/media/m2.48907311.jpg"
+  },
+  {
+    "revision": "d177c3278a875bb54fd6a9c39032496b",
+    "url": "/2020/static/media/n.d177c327.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c109bfda73adda808cfee98b5d65d015.js"
+  "/2020/precache-manifest.abbd4a9dcc367d2b16ba7ce916977fb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
